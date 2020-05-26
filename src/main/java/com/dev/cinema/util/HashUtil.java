ff263be5916303ca685a534659bd36db631f75b3
ana-dav <opa.super.op@gmail.com>
2020-05-26 10:39:13 +0300

    mvn

--- src/main/java/com/dev/cinema/util/HashUtil.java
+++ src/main/java/com/dev/cinema/util/HashUtil.java
@@ -1,7 +1,6 @@
 package com.dev.cinema.util;
 
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
