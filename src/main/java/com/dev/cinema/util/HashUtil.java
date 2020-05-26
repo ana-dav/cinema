818e19f2602f95b630c051c153d628c5e05afdfe
ana-dav <opa.super.op@gmail.com>
2020-05-26 10:36:47 +0300

    HashUtil

--- src/main/java/com/dev/cinema/util/HashUtil.java
+++ src/main/java/com/dev/cinema/util/HashUtil.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 public class HashUtil {
     private static final Logger LOGGER = LogManager.getLogger(HashUtil.class);
+    private static final String CRYPTOGRAPHY = "SHA-512";
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
@@ -19,13 +20,13 @@ public class HashUtil {
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
+            MessageDigest messageDigest = MessageDigest.getInstance(CRYPTOGRAPHY);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b: digest) {
                 hashedPassword.append(String.format("%02x", b));
             }
-        } catch (NoSuchAlgorithmException e) {
+        } catch (Exception e) {
             LOGGER.error(e);
         }
         return hashedPassword.toString();
